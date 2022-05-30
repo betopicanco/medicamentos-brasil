feae51fe33808a8d27cbacc431dd91a1e53514a8
betopicanco <betopicanco@gmail.com>
2022-05-29 22:34:39 -0300

    add /meds

--- backend/src/Meds/interface.ts
+++ backend/src/Meds/interface.ts
@@ -1,7 +1,12 @@
 interface medInterface {
-  substancia: string,
-  // produto: string,
-  // preco: string
+  substance: string,        // SUBSTÂNCIA
+  barCode: string,          // EAN 1
+  product: string,          // PRODUTO
+  presentation: string,     // APRESENTAÇÃO
+  pfTaxFree: number | '' | undefined,        // PF Sem Impostos
+  pmc0Percent: number | '' | undefined,      // PMC 0%
+  listOfTaxCreditGranting: string,  // LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO PISCOFINS:
+  commerc2020: string,      // COMERCIALIZAÇÃO 2020
 }
 
 export default medInterface;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,78 +1,54 @@
 import { Router } from 'express';
 
-// Middleware para lidar com multipart/form-data:
-import multer from "multer";
-
-import { Readable } from "stream";
-
-// Prove interface para litura de dados do Readable
-import readLine from "readline";
-
 import medInterface from './Meds/interface';
 import medHeader from './Meds/header';
 
 const router = Router();
-const multerConfig = multer();
 
 router.post(
   '/meds',
-  async (req: any, res: any) => {
-    const medsJson = req.body
-    const meds = JSON.stringify(medsJson)
-    console.log(meds);
+  async (req: any, res: any, next: any) => {
+    const propToNumber = (value: string, prop: string) => {
+      if(value === '') return 0;
 
-    medHeader.forEach((header) => {
-      // if(!(header in  JSON.parse(medsJson))) console.log(`falta o header ${header}`)
-    })
+      const number = Number(value.replace(',', '.'));
 
-    return res.json({test: 'ok'})
+      if(isNaN(number)) throw `${prop} informado contém um valor inválido`;
+
+      return number;
+    }
+
+    try {
+      const medsJson:JSON = req.body
+      const medsString = JSON.stringify(medsJson);
+      const medsList = JSON.parse(medsString);
+
+      medHeader.forEach((header) => {
+        if(!(header in  medsList[0])) throw `Coluna ${header} não encontrada!`;
+      });
+
+      const meds:medInterface[] = [];
+
+      medsList.forEach((med: any) => {
+        meds.push({
+          substance: med['SUBSTNCIA'],
+          barCode: med['EAN 1'],
+          product: med['PRODUTO'],
+          presentation: med['APRESENTAO'],
+          pfTaxFree: propToNumber(med['PF Sem Impostos'], 'PF Sem Impostos'),
+          pmc0Percent: propToNumber(med['PMC 0'], 'PMC 0'),
+          listOfTaxCreditGranting: med['LISTA DE CONCESSO DE CRDITO TRIBUTRIO PISCOFINS'],
+          commerc2020: med['COMERCIALIZAO 2020']
+        });
+      });
+
+      return res.json({meds});
+    } catch(err: any) {
+      console.error(err);
+
+      return res.status(500).send({ error: err });
+    }
   }
 );
-
-// router.post(
-//   '/meds',
-//   multerConfig.single('file'),  // Recebe um arquivo e nomeia file
-//   async (request: any, response: any) => {
-//     const { file } = request;
-//     const { buffer } = file;
-
-//     const readableFile = new Readable();
-//     readableFile.push(buffer);
-//     readableFile.push(null)
-
-//     const medsLine = readLine.createInterface({
-//       input: readableFile
-//     });
-
-
-//     const meds: medInterface[] = [];
-//     let cont = 0;
-
-//     for await(let line of medsLine) {
-//       if(cont === 0) {
-//         cont++;
-//       } 
-//       // const medsLineSplit = line.split(';');
-
-//       // if(cont == 0) {
-//       //   medHeader.forEach((header: string, index: number) => {
-//       //     const headerWhithoutAccent = medsLineSplit[index].replace(
-//       //       /([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, 
-//       //       ''
-//       //     );
-//       //     // if(header !==  medsLineSplit[index]) console.log(`erro no header ${head}`);
-//       //   });
-
-//       //   cont++;
-//       // } else {
-//       //   meds.push({
-//       //     substancia: medsLineSplit[0],
-//       //   });
-//       // }
-//     }
-
-//     return response.json({test: 'ok'});
-//   }
-// );
 
 export { router };
